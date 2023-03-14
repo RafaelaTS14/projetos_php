43bf8ec3e83a71db45a0275047b743940cd4fa76
Rafaela Taschner Shcimaia <rafa456.rts@gmail.com>
2023-03-14 15:26:08 -0300

    loop

--- loops/index.php
+++ loops/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$num1 = 3;
+/*$num1 = 3;
 $num2 = 4;
 
 //$resultado = 3 * 4;
@@ -11,5 +11,15 @@ for($i = 0; $i < $num2; $i++) {
     $resultado += $num1;
 }
 
-echo 'O resultado da multiplicação é: ' . $resultado; /* o ponto é usado
+echo 'O resultado da multiplicação é: ' . $resultado; */
+/* o ponto é usado
 pra concatenar no php */
+//$i é definido pra variável que inicia o loop no for, não é obrigatório mas é bom usar i;
+// o < $num2 é a condição pra parar o loop.
+// o $i++ serve pra acrescentar mais um no valor
+
+$i = 0;
+do {
+    $i++;
+} while (false);
+echo $i;

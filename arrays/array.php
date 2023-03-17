3e23d00bfb1cd27498b5d94ad3e21cb6dce48305
Rafaela Taschner Shcimaia <rafa456.rts@gmail.com>
2023-03-17 08:49:14 -0300

    aula de array

---
+++ arrays/array.php
@@ -0,0 +1,8 @@
+<?php
+
+$lista = array(1, 2, 3, 4);
+$lista[] = 5; //se colocar um número no colchete, ele define o valor depois do igual como índice
+
+var_dump($lista);
+
+//arrays são usados, principalmente, pra organizar dados.

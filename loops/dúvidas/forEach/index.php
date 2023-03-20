e0a03ad45bed6eef2b5eeac5864581696f639f58
Rafaela Taschner Shcimaia <rafa456.rts@gmail.com>
2023-03-20 13:31:16 -0300

    loops: foreach

---
+++ loops/dúvidas/forEach/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$transp = ["Carro", "Moto", "Bicicleta", "Ônibus", "Avião", "Navio"];
+
+foreach ($transp as $veiculo) {    //para cada elemento do array $transp armazenado em $veiculo
+    echo $veiculo . "<br>";
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loop ForEach</title>
+</head>
+
+<body>
+
+</body>
+
+</html>

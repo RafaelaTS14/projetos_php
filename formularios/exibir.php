306018b247d3093a2bbfab9ef42f3bbc32a85730
Rafaela Taschner Shcimaia <rafa456.rts@gmail.com>
2023-03-20 17:21:48 -0300

    formulários em HTML com método $_GET

---
+++ formularios/exibir.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "O nome é: " . $_GET['nome'] . '<br>';
+echo "A idade é: " . $_GET['idade'] . '<br>';
+echo "O e-mail é: " . $_GET['email'] . '<br>';
---
+++ formularios/noticia.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+
+</head>
+
+<body>
+    <div class="center">
+        <form method="GET" action="exibir.php">
+            Nome: <input type="text" name="nome"><br><br>
+            Idade: <input type="number" name="idade"><br><br>
+            E-mail: <input type="text" name="email"><br><br>
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
+</body>
+
+</html>

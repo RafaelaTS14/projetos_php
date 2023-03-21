57e2ead3fd65e8675f84862e61b6aea44f896dab
Rafaela Taschner Shcimaia <rafa456.rts@gmail.com>
2023-03-21 09:38:08 -0300

    adição de css

--- formularios/exibir.php
+++ formularios/exibir.php
@@ -1,5 +1,50 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
 
-echo "O nome é: " . $_GET['nome'] . '<br>';
-echo "A idade é: " . $_GET['idade'] . '<br>';
-echo "O e-mail é: " . $_GET['email'] . '<br>';
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados</title>
+</head>
+<div class='center'>
+
+    <head>
+        <style>
+            .center {
+                margin: auto;
+                text-align: center;
+                width: 30%;
+                padding: 12px;
+            }
+
+            a {
+                color: #7AA9EB
+            }
+
+            h1 {
+                color: white
+            }
+
+            b {
+                color: white
+            }
+        </style>
+    </head>
+
+    <body style="background-color:#070C0F;">
+
+        <h1 style="font-family:signika negative;" style: .center { margin: auto; width: 50%; padding: 10px; }> Dados do usuário: </h1>
+        <a style="font-family:signika negative;">
+            <b style="font-family:signika negative;">
+                <?php
+
+                echo "Nome de usuário: " . "</b>" . $_GET['nome'] . '<br>';
+                echo "<b>" . "Idade do usuário: " . "</b>" . $_GET['idade'] . '<br>';
+                echo "<b>" . "E-mail do usuário: " . "</b>" . $_GET['email'] . '<br>';
+
+                ?>
+        </a>
+    </body>
+
+</html>
--- formularios/noticia.php
+++ formularios/noticia.php
@@ -9,38 +9,44 @@
 
 </head>
 <div class='center'>
+
     <head>
         <style>
- .center {
-  margin: auto;
-  text-align:center;
-  border: 3px solid #B4D3B2;
-  border-radius: 2.0rem;
-  box-shadow: 10px 10px 5px #C7D7C9;
-  width: 50%;
-  padding: 10px;
-}
-</style>
-    </head>
-<body style="background-color:#D6E6DF;">
+            .center {
+                margin: auto;
+                text-align: center;
+                width: 30%;
+                padding: 10px;
+            }
 
-<p style="font-family:verdana;"
-style:
-.center {
-  margin: auto;
-  width: 50%;
-  padding: 10px;
-}> Preencha os dados abaixo: </p> 
-<b style="font-family:verdana;">
-<body>
-    <div class="center">
+            a {
+                color: #7AA9EB
+            }
+
+            h1 {
+                color: white
+            }
+
+            b {
+                color: white
+            }
+        </style>
+    </head>
+
+    <body style="background-color:#070C0F;">
+
+        <h1 style="font-family:signika negative;" style: .center { margin: auto; width: 50%; padding: 10px; }> Preencha os dados abaixo: </h1>
+        <b style="font-family:signika negative;">
+    </body>
+    <a style="font-family:signika negative;">
         <form method="GET" action="exibir.php">
             Nome: <input type="text" name="nome"><br><br>
             Idade: <input type="number" name="idade"><br><br>
             E-mail: <input type="text" name="email"><br><br>
-            <button type="submit">Enviar</button>
+            <button style="font-family:signika negative;" type="submit">Enviar</button>
         </form>
-    </div>
+    </a>
+</div>
 </body>
 
 </html>
